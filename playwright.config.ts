bd19829357dedaf2c13f4f5f9aae5ba81a67c3f6
Erik Ccanto <ccanto.erik@gmail.com>
2024-01-06 18:24:43 -0300

    refactor: restore chromium and safari desktop browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,17 @@ import { defineConfig, devices } from '@playwright/test'
 
 export default defineConfig({
     testDir: './tests',
-    reporter: [['html', { open: 'never' }]],
+    reporter: [
+        ['html', { open: 'never' }]
+    ],
     projects: [
         {
-            name: 'Google Chrome',
-            use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
         },
         {
-            name: 'Microsoft Edge',
-            use: { ...devices['Desktop Edge'], channel: 'msedge' },
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
         },
     ],
     workers: 4,
